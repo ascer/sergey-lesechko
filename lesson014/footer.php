243bfd6e69c953f18ce2216e34ee6d2aa0cec1aa
Sergey <less@xlan.net.ua>
2013-10-23 21:16:45 +0300

    Commit

---
+++ lesson014/footer.php
@@ -0,0 +1,12 @@
+	<div id="footer">
+		<ul>
+        	<li>Главная</li>
+        	<li class="act">О GeekHub</li>
+        	<li>Команда</li>
+        	<li>Контакты</li>
+        </ul>
+        <p>© Copyright 2011</p>
+	</div>
+</div>
+</body>
+</html>
---
+++ lesson014/index.php
@@ -0,0 +1,27 @@
+<?php get_header(); ?>
+	<div id="content">
+		<h2>Знай наших!</h2>
+        <ul>
+            <li>
+                <div class="left-fl"><h3>Хытыемель Цыэсесенко</h3><span>Всея кодудер</span></div>
+                <img class="left-fl" src="images/photo-01.png" title="Хытыемель Цыэсесенко" alt="Хытыемель Цыэсесенко" />
+                <p class="left-fl">Rostrud tionsequat. Met lan et, vullummy num augait incipit vullamet praestrud dunt lore conse con henibh ex ectem alit velisl ex eratue consequatum zzrit duisit irit lut volore ecte exerci te faci ea feui eugait aut lore dolendigna feugiamet ea autpatuero odo dolorem iliquam, velesed del ulputetue modit nisi.</p>
+            </li>
+            <li>
+                <div class="right-fl"><h3>Хытыемель Цыэсесенко</h3><span>Всея кодудер</span></div>
+                <img class="right-fl" src="images/photo-01.png" title="Хытыемель Цыэсесенко" alt="Хытыемель Цыэсесенко" />
+                <p class="right-fl">Rostrud tionsequat. Met lan et, vullummy num augait incipit vullamet praestrud dunt lore conse con henibh ex ectem alit velisl ex eratue consequatum zzrit duisit irit lut volore ecte exerci te faci ea feui eugait aut lore dolendigna feugiamet ea autpatuero odo dolorem iliquam, velesed del ulputetue modit nisi.</p>
+            </li>
+            <li>
+                <div class="left-fl"><h3>Хытыемель Цыэсесенко</h3><span>Всея кодудер</span></div>
+                <img class="left-fl" src="images/photo-01.png" title="Хытыемель Цыэсесенко" alt="Хытыемель Цыэсесенко" />
+                <p class="left-fl">Rostrud tionsequat. Met lan et, vullummy num augait incipit vullamet praestrud dunt lore conse con henibh ex ectem alit velisl ex eratue consequatum zzrit duisit irit lut volore ecte exerci te faci ea feui eugait aut lore dolendigna feugiamet ea autpatuero odo dolorem iliquam, velesed del ulputetue modit nisi.</p>
+            </li>
+            <li>
+                <div class="right-fl"><h3>Хытыемель Цыэсесенко</h3><span>Всея кодудер</span></div>
+                <img class="right-fl" src="images/photo-01.png" title="Хытыемель Цыэсесенко" alt="Хытыемель Цыэсесенко" />
+                <p class="right-fl">Rostrud tionsequat. Met lan et, vullummy num augait incipit vullamet praestrud dunt lore conse con henibh ex ectem alit velisl ex eratue consequatum zzrit duisit irit lut volore ecte exerci te faci ea feui eugait aut lore dolendigna feugiamet ea autpatuero odo dolorem iliquam, velesed del ulputetue modit nisi.</p>
+            </li>
+        </ul>
+	</div>
+<?php get_footer(); ?>
